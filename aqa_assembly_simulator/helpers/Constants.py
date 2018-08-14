31eb6d776d3b53529ecaf2f4f6852d5f2e255839
johnyob <johnyob132@gmail.com>
2018-08-14 22:50:12 +0100

    Update Constants.py. Fixed linux path issue.

--- aqa_assembly_simulator/helpers/Constants.py
+++ aqa_assembly_simulator/helpers/Constants.py
@@ -1,10 +1,12 @@
 import os
 
-from aqa_assembly_simulator import ROOT
+from aqa_assembly_simulator import ROOT, OS
 
-SYNTAX_JSON = os.path.join(ROOT, "parser\\syntax.json")
+separator = "\\" if OS == "nt" else "/"
+
+SYNTAX_JSON = os.path.join(ROOT, "parser{0}syntax.json".format(separator))
 COMMANDS_JSON = os.path.join(ROOT, "commands.json")
-VM_CONFIG = os.path.join(ROOT, "virtual_machine\\config\\config.json")
+VM_CONFIG = os.path.join(ROOT, "virtual_machine{0}config{0}config.json".format(separator))
 
 REGISTERS_REGEX = r"^(\d{1,2})$"
 MEMORY_CAPACITY_REGEX = r"^(\d{1,3})$"
