bc564b4f3a3eb1d2bec5d353861a7449641a7c3e
johnyob <johnyob132@gmail.com>
2018-08-17 16:57:48 +0100

    Update ParseError.py. Added report method.

--- aqa_assembly_simulator/error/ParseError.py
+++ aqa_assembly_simulator/error/ParseError.py
@@ -5,3 +5,6 @@ class ParseError(RuntimeError):
 
     def __init__(self, token, message):
         super().__init__(token, message)
+
+    def report(self):
+        return "[ERROR] Error: AssemblySimulatorParseError, Response: ({0})".format(super().report())
