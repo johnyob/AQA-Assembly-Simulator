1b953c0ca999c6dc2db969fc958cac62f7a9697e
johnyob <johnyob132@gmail.com>
2018-08-17 16:57:36 +0100

    Update VirtualMachineError.py. Added report method.

--- aqa_assembly_simulator/error/VirtualMachineError.py
+++ aqa_assembly_simulator/error/VirtualMachineError.py
@@ -5,3 +5,6 @@ class VirtualMachineError(RuntimeError):
 
     def __init__(self, token, message):
         super().__init__(token, message)
+
+    def report(self):
+        return "[ERROR] Error: AssemblySimulatorVMError, Response: ({0})".format(super().report())
