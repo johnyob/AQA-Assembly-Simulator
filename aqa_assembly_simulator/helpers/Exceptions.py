2d42908d538c9296e29084b900f4dcae93b4fa2c
johnyob <johnyob132@gmail.com>
2018-08-17 16:56:34 +0100

    Update Exceptions.py. Added __str__ method.

--- aqa_assembly_simulator/helpers/Exceptions.py
+++ aqa_assembly_simulator/helpers/Exceptions.py
@@ -1,2 +1,4 @@
 class AssemblySimulatorVMConfigException(Exception):
-    pass
+
+    def __str__(self):
+        return "[ERROR] Error: AssemblySimulatorConfigError, Response: {0}".format(super().__str__())
